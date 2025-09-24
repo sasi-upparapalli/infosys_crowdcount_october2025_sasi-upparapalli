92ec0bd9be7e092350e851a4191786c0add00e16
UPPARAPALLI SURYA SASIKANTH <ussk2284@gmail.com>
2025-09-24 22:03:04 +0530

    Create Task4-String Operators.py

---
+++ Week1/Day02 /Task4-String Operators.py
@@ -0,0 +1,9 @@
+s = "Hello"
+t = "World"
+
+print(s + t)        # HelloWorld
+print(s * 3)        # HelloHelloHello
+print(s[1])         # e
+print(s[1:4])       # ell
+print("e" in s)     # True
+print("z" not in s) # True
