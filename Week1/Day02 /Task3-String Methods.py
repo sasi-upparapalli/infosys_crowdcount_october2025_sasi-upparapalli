86f01c358a75b0d92426916552af6cf6687a5747
UPPARAPALLI SURYA SASIKANTH <ussk2284@gmail.com>
2025-09-24 22:02:31 +0530

    Create Task3-String Methods.py

---
+++ Week1/Day02 /Task3-String Methods.py
@@ -0,0 +1,8 @@
+text = "Infy Springboard"
+
+print(text.lower())           # infy springboard
+print(text.upper())           # INFY SPRINGBOARD
+print(text.strip())           # Infy Springboard
+print(text.replace("Infy", "Infosys"))   # Infosys Springboard
+print(text.split())           # ['Infy', 'Springboard']
+print(len(text))              # 16
