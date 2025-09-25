24cdae572cb4b494b586777fa1a343ac3f3334ba
UPPARAPALLI SURYA SASIKANTH <ussk2284@gmail.com>
2025-09-25 22:12:41 +0530

    Update task2-functions-manipulating-lists.py

--- Week1/Day03/task2-functions-manipulating-lists.py
+++ Week1/Day03/task2-functions-manipulating-lists.py
@@ -1,1 +1,21 @@
 
+data = [1, 2, 'abc', 3, 4]
+
+print("Original list:", data)
+
+data.append('new')               # Add an item
+print("After append:", data)
+
+data.remove(2)                   # Remove an item
+print("After removing 2:", data)
+
+data[0] = 'first'                # Modify an item
+print("After modifying index 0:", data)
+
+print("Reversed list:", data[::-1])  # Reverse the list
+
+item = 'abc'
+if item in data:
+    print("Index of", item, ":", data.index(item))
+else:
+    print(item, "not found in list.")
